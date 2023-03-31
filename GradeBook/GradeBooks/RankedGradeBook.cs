3f4932f5518ebc5a8cd94a242346329feff90b09
dyloo98 <dawid.dyl2@wp.pl>
2023-03-31 19:08:00 +0200

    Override RankedGradeBook's CalculateStatistics method Override RankedGradeBook's CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -27,5 +27,24 @@ namespace GradeBook.GradeBooks
             else
                 return 'A';
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
