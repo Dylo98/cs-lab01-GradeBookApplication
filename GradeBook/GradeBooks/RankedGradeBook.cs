bd43b83545ecd9ba798bb8cf7b68d49d37b769d7
dyloo98 <dawid.dyl2@wp.pl>
2023-03-31 20:16:30 +0200

    Update BaseGradeBook.GetGPA
    Fix RankedGradeBook and StandardGradeBook (internal class => public)

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
